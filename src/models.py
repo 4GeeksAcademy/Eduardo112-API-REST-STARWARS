dca9a2695c8713b89c3f9c757a3340248df2e7ea
Incerteza88 <info@incerteza.es>
2025-07-26 18:35:23 +0000

    Inicio proyecto

--- src/models.py
+++ src/models.py
@@ -1,15 +1,21 @@
 from flask_sqlalchemy import SQLAlchemy
 from sqlalchemy import String, Boolean
-from sqlalchemy.orm import Mapped, mapped_column
+from sqlalchemy.orm import Mapped, mapped_column, relationship
+from datetime import datetime
+from typing import List
 
 db = SQLAlchemy()
 
 class User(db.Model):
+    __tablename__= "user"
     id: Mapped[int] = mapped_column(primary_key=True)
     email: Mapped[str] = mapped_column(String(120), unique=True, nullable=False)
     password: Mapped[str] = mapped_column(nullable=False)
     is_active: Mapped[bool] = mapped_column(Boolean(), nullable=False)
-
+    registration_date: Mapped[datetime] = mapped_column(nullable=False)    # se genera solo
+    name: Mapped[str] = mapped_column(db.String(120), nullable=False)
+    last_name: Mapped[str] = mapped_column(db.String(120), nullable=False)
+    favorites: Mapped[list["FavoritesList"]] = relationship("FavoritesList", back_populates="user", lazy="select")
 
     def serialize(self):
         return {
@@ -17,3 +23,10 @@ class User(db.Model):
             "email": self.email,
             # do not serialize the password, its a security breach
         }
+
+class FavoritesList(db.Model):
+    __tablename__ = "favoritelist"
+    id: Mapped[int] = mapped_column(primary_key=True)
+    user_id: Mapped[int] = mapped_column(db.ForeignKey("user.id"), nullable=False)
+   
+    user: Mapped["User"] = relationship("User", back_populates="favorites")
